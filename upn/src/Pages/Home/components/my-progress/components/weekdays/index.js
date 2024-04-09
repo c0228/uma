cd119a57ea61fe463c6ab097c2ba6e3d5dc06439
Nellutla L N Rao <nlnrao228@gmail.com>
2024-04-09 21:08:05 +0530

    Updated Projects

---
+++ upn/src/Pages/Home/components/my-progress/components/weekdays/index.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+export const WeekDatesComponent = () => {
+  const [weekDates, setWeekDates] = useState([]);
+  const [todayDateView, setTodayDateView] = useState('');
+
+  useEffect(() => {
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const monthsOfYear = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    
+    const today = new Date();
+    const todayWeek = daysOfWeek[today.getDay()];
+    const todayDate = today.getDate();
+    const todayMonth = monthsOfYear[today.getMonth()];
+    const todayYear = today.getFullYear();
+    setTodayDateView(todayWeek+', '+todayDate+' '+todayMonth+' '+todayYear);
+    const currentDay = today.getDay(); // 0 for Sunday, 1 for Monday, ..., 6 for Saturday
+    const startDate = new Date(today);
+    startDate.setDate(today.getDate() - currentDay); // Set start date to Sunday of the current week
+    
+    let dates = [];
+    for (let i = 0; i < 7; i++) {
+      const date = new Date(startDate);
+      date.setDate(startDate.getDate() + i);
+      const isToday = date.toDateString() === today.toDateString();
+      const day = daysOfWeek[date.getDay()];
+      const formattedDate = date.toLocaleDateString('en-GB', { day: '2-digit' });
+      
+      dates.push({
+        day: day?.substring(0, 3),
+        date: formattedDate,
+        isToday: isToday
+      });
+    }
+    
+    setWeekDates(dates);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.todayDateView}>
+        <FontAwesome name="calendar" size={20} color="black" style={styles.todayDateIcon}/>
+        <Text style={styles.todayText1}>Today -</Text>
+        <Text style={styles.todayText2}>{todayDateView}</Text>
+      </View>
+      <View style={styles.flexContainer}>
+        {weekDates.map((item, index) => (
+          <View key={index} style={[styles.flexItem, (item.isToday ? styles.flexItemActive : null)]}>
+            <Text style={styles.flexItemDay}>{item.day}</Text>
+            <Text style={styles.flexItemDate}>{item.date}</Text>
+          </View>
+        ))}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  todayDateView:{ flex:1, flexDirection:'row', marginBottom: 10, justifyContent:'center' },
+  todayDateIcon:{ marginTop:-2 },
+  todayText1:{ fontWeight: 'bold', marginBottom: 5, fontSize:14, color:'#333', marginLeft: 5, marginRight:5, color:'black'  },
+  todayText2:{ fontSize:14, color:'#555', marginLeft: 5 },
+  flexContainer: { flexDirection: 'row', justifyContent: 'space-between' },
+  flexItem: { alignItems:'center', flex: 1, padding: 5, marginTop: 5, marginRight: 5, backgroundColor: '#f5f5f5', borderRadius: 8 },
+  flexItemActive: { borderColor: 'tomato', borderWidth: 1 },
+  flexItemDay: { textTransform: 'uppercase', fontSize:11, color:'#333', fontWeight:'bold' },
+  flexItemDate:{ color:'#555' }
+});
+
--- upn/src/Pages/Home/components/my-progress/index.js
+++ upn/src/Pages/Home/components/my-progress/index.js
@@ -1,19 +1,23 @@
 import React from "react";
 import { View, Text, ScrollView, StyleSheet } from "react-native";
 import { HamburgerIcon } from './../../../../Navigation/Drawer/index.js';
+import { WeekDatesComponent } from './components/weekdays/index.js';
 
 const MyProgress = (props)=>{
  return (<View style={MyProgressStyle.pageView}>
  <HamburgerIcon {...props}/>      
  <ScrollView style={MyProgressStyle.scrollView}>
- <Text>MyProgress Page</Text>
+   <Text style={[MyProgressStyle.mainTitle, MyProgressStyle.textCenter]}>My Progress for the Week</Text>
+   <WeekDatesComponent />
  </ScrollView>
  </View>);
 };
 
 const MyProgressStyle = StyleSheet.create({ 
  pageView: {  flex:1, backgroundColor:'#fff' },
- scrollView: { paddingLeft:15, paddingRight: 15 }
+ scrollView: { paddingLeft:15, paddingRight: 15 },
+ mainTitle: { fontSize:18, paddingBottom:15, fontWeight:'bold', color:'#000', lineHeight: 22 },
+ textCenter: { textAlign: 'center' }
 });
 
 export default MyProgress;
