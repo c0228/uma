d4f67a783f2b9f5b1a8007d3dd4e4a55b42fe285
Nellutla L N Rao <nlnrao228@gmail.com>
2024-04-10 01:58:24 +0530

    Updated Projects

--- upn/src/Pages/Home/components/notifications/index.js
+++ upn/src/Pages/Home/components/notifications/index.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { View, Text, ScrollView, StyleSheet } from "react-native";
 import { HamburgerIcon } from './../../../../Navigation/Drawer/index.js';
+import { useWindowDimensions } from 'react-native';
+import RenderHtml from 'react-native-render-html';
+
+const source = {
+    html: `
+  <p style='text-align:center;background-color:orange;'>
+    Hello World!
+  </p>`
+  };
 
 const Notifications = (props)=>{
+    const { width } = useWindowDimensions();
  return (<View style={NotifyStyle.pageView}>
  <HamburgerIcon {...props}/>      
  <ScrollView style={NotifyStyle.scrollView}>
  <Text>Notification Page</Text>
+ <RenderHtml
+      contentWidth={width}
+      source={source}
+    />
  </ScrollView>
  </View>);
 };
