3df624d792fcc0fd6a601a4e2005e72921a0c302
Nellutla L N Rao <nlnrao228@gmail.com>
2024-05-17 20:36:26 +0530

    Updated Projects

--- upn/src/Pages/Splash/components/time-table/index.js
+++ upn/src/Pages/Splash/components/time-table/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, ScrollView } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import AlertModal from '@AppComponent/AlertModal/index.js';
 import BEHeader, { HeaderTitle } from './../../utils/BEHeader.js';
@@ -13,7 +13,13 @@ const TimeTable = () =>{
             title="Set your Weekly Availability" 
             subTitle="Specify the Examinations that you are planning to pursue in the Upcoming Years -" />
     <ScrollView style={{ paddingLeft:5, marginBottom:5, paddingRight:5 }}>
-
+      <View style={{ padding: 15 }}>
+        <View style={{ flexDirection:'row' }}>
+          <View style={{ width:'30%' }}>
+            <Text style={{ color:'#000', fontWeight:'bold', fontSize:15 }}>Monday</Text>
+          </View>
+        </View>
+      </View>
     </ScrollView>
     <BEFooter 
         previousForm={()=>{
