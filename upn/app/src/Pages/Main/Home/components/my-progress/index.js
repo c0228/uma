7e61bd7719837e5f93f013622b26f078faf48ae3
github-tejaswi <tkukalakunta@gmail.com>
2024-08-05 18:05:19 +0530

    Updated projects

--- upn/app/src/Pages/Main/Home/components/my-progress/index.js
+++ upn/app/src/Pages/Main/Home/components/my-progress/index.js
@@ -1,17 +1,19 @@
 import React, { useEffect } from 'react';
 import { View, Text, ScrollView, StyleSheet } from 'react-native';
 import Page from '@AppUtils/Page.js';
+import { WeekDatesComponent } from './components/weekdays/index.js';
 
 const MyProgress = () =>{
   return (<Page backgroundColor="#0e7512" title="My Progress">
-    <View>
-      <Text>Progress Test</Text>
-    </View>
+  <ScrollView style={styles.scrollView}>
+   <WeekDatesComponent />
+  </ScrollView>
   </Page>);
 };
 
+
 const styles = StyleSheet.create({
-  
-});
+  scrollView: { paddingLeft:15, paddingTop:15, paddingRight: 15 }
+ });
 
 export default MyProgress;
